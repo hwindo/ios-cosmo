1750be6e67f937e71b93f1fa2dd0a8481fae27a4
windo <herwindo.artono@gmail.com>
2021-03-27 12:24:24 +0700

    Initial Commit

---
+++ ios-cosmo-app/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ios-cosmo-app
+//
+//  Created by Herwindo Artono on 27/03/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
